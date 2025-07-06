3d24c67274c20d6023f0b0985a7633f4c4be5828
yashagr0089 <yashagrawal0110@gmail.com>
2025-07-07 00:42:05 +0530

    Update entrypoint.sh

--- my-docker-action/entrypoint.sh
+++ my-docker-action/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh -l
 
-echo "Hello, $1!"
+echo "Hello, $INPUT_PERSON_NAME!"
 time=$(date)
 echo "The current time is $time"
-echo "greeting=Hello, $1! Time is $time" >> $GITHUB_OUTPUT
+
+# Make sure GITHUB_OUTPUT exists
+mkdir -p "$(dirname "$GITHUB_OUTPUT")"
+
+echo "greeting=Hello, $INPUT_PERSON_NAME! Time is $time" >> "$GITHUB_OUTPUT"
+
